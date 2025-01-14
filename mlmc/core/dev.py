9a1dc94f494f5256ca439c035b58145972341571
philippweder <philippweder@hotmail.com>
2025-01-14 15:24:14 +0100

    chore: clean up unused files

--- mlmc/core/dev.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-from mlmc.core.estimators import standard_mc
-from mlmc.core.options import AsianOption
-
-nsamp = 1000
-h = 0.1
-option = AsianOption()
-result = standard_mc(nsamp, h, option)
